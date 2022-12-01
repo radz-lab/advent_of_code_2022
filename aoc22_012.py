2a4ad5f4854a5a40955c50c38afc36dcdf107cc2
radz-lab <119614525+radz-lab@users.noreply.github.com>
2022-12-01 17:29:37 +0000

    Add files via upload

--- aoc22_012.py
+++ aoc22_012.py
@@ -4,31 +4,28 @@ elf_calories = input_file.readlines()
 import array
 
 max_calories = 0
-top_three_elf_calories = [0,0,0]
+top_three_elf_calories = [0, 0, 0]
 current_elf_calories: int = 0
 
 for line in elf_calories:
-    if line.strip() == '':
+    try:
+        calories_on_line = float(line)
+        current_elf_calories += calories_on_line
+    except:
         if current_elf_calories > top_three_elf_calories[2]:
             if current_elf_calories > top_three_elf_calories[1]:
                 if current_elf_calories > top_three_elf_calories[0]:
-                    top_three_elf_calories[2]=top_three_elf_calories[1]
-                    top_three_elf_calories[1]=top_three_elf_calories[0]
-                    top_three_elf_calories[0]=current_elf_calories
+                    top_three_elf_calories[2] = top_three_elf_calories[1]
+                    top_three_elf_calories[1] = top_three_elf_calories[0]
+                    top_three_elf_calories[0] = current_elf_calories
                 else:
-                    top_three_elf_calories[2]=top_three_elf_calories[1]
-                    top_three_elf_calories[1]=current_elf_calories
+                    top_three_elf_calories[2] = top_three_elf_calories[1]
+                    top_three_elf_calories[1] = current_elf_calories
             else:
-                top_three_elf_calories[2]=current_elf_calories
+                top_three_elf_calories[2] = current_elf_calories
             current_elf_calories = 0
         else:
             current_elf_calories = 0
-    else:
-        try:
-            calories_on_line = float(line)
-            current_elf_calories += calories_on_line
-        except:
-            pass
 
 max_calories = sum(top_three_elf_calories)
 print(max_calories)
